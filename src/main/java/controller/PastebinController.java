0849ac03daa668e6698581f83d7eca3998a151f5
parinovK <parinovk@yandex.ru>
2024-03-18 22:34:33 +0300

    added Controller - PastebinController

--- src/main/java/controller/PastebinController.java
+++ src/main/java/controller/PastebinController.java
@@ -1,5 +1,15 @@
 package controller;
 
-public class PastebinController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("api/v1/pastebin")
+public class PastebinController {
+    @GetMapping("{hash}")
+    public String getByHash(@PathVariable String hash){
+        return "test App";
+    }
 }

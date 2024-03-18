65f047c872b96120e1820a5939cd306cb05c4310
parinovK <parinovk@yandex.ru>
2024-03-18 23:30:45 +0300

    added model "PastebinModel" and rewrite controller "PastebinController"

--- src/main/java/controller/PastebinController.java
+++ src/main/java/controller/PastebinController.java
@@ -1,15 +1,25 @@
 package controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import model.PastebinModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/pastebin")
 public class PastebinController {
+    @GetMapping
+    public List<String> getPasteOfList(){
+        return Collections.emptyList();
+    }
     @GetMapping("{hash}")
     public String getByHash(@PathVariable String hash){
         return hash;
+    }
+
+    @PostMapping("save_paste")
+    public String add (@RequestBody PastebinModel pastebin){
+        return pastebin.getData();
     }
 }

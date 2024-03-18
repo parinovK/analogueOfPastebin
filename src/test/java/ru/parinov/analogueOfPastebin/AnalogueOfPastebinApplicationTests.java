4d23f331f00c7491fd8b6fe82c6f14c7adb0ee6f
parinovK <parinovk@yandex.ru>
2024-03-18 22:23:41 +0300

    init

---
+++ src/test/java/ru/parinov/analogueOfPastebin/AnalogueOfPastebinApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.parinov.analogueOfPastebin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalogueOfPastebinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
